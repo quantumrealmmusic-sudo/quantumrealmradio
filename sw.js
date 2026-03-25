2c81483d6fc9c9a497a62555dc560168c5a59912
quantumrealmmusic-sudo <quantumrealmmusic@gmail.com>
2026-03-25 15:49:38 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,31 @@
 
+const CACHE_NAME = 'quantum-radio-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
